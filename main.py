10055a3c5814e1227e53662838c52cc1c8392128
Ayaee <61822669+Ayaee@users.noreply.github.com>
2023-03-24 13:19:50 +0100

    Existing folder not empty function (#2)
    * update: copy_before_save / add existing, empty
    
    update du nom de la fonction 'copy_before_save',
    delete de la variable 'rename' pour la mettre dans le test (pour au final la faire inscrire par l'utilisateur avant dans une IU)
    ajout d'un if pour raise une error si l'utilisateur met un nom qu'avec des espaces
    ajout d'un if pour vérifier si le folder choisit par l'utilisateur est existant (grâce à la fonction 'existing')
    
    ajout de la fonction 'existing' pour vérifier si le folder choisit par l'utilisateur est existant si non il raise une Error indiquant le nom invalide du folder
    
    ajout d'une fonction 'empty' vide
    
    update erreur 'RenameFailure' n'est pas une def mais une class pour écrire les erreurs
    
    * Update: main
    
    update du if de vérification avant de créer le template du renommage (vérification dossier existant et vide ou non)
    add de la fonction empty permettant de vérifier si le dossier contient ou non des fichiers à renommer à l'intérieur
    
    * update: main
    
    add/update fonction 'existing' et 'empty' combinées pour créer 'check_existing_not_empty' servant à réaliser les deux check en même temps
    add docstring 'check_existing_not_empty'

--- main.py
+++ main.py
@@ -8,33 +8,58 @@ import lucidity
 import conf
 
 
-def rename_copy_before_save():
+def copy_before_save(rename):
     """
     Fonction servant à renommer les fichiers dans 'before' pour les copier intégralement dans 'after'.
     Cette fonction ne supprime pas le dossier 'before' et garde les deux dossiers.
 
     :return: La totalité des fichiers dans 'before' renommée comme désirée dans 'after'
     """
+    # rename = input("Quel sera le nouveau nom de vos différentes frames ?\n")  # masterlayer.0230.exr
     img_template = lucidity.Template("rename", conf.rename_pattern, anchor=lucidity.Template.ANCHOR_END)
 
-    name = input("Quel sera le nouveau nom de vos différentes frames ?\n")  # masterlayer.0230.exr
+    if rename.strip() == "":
+        raise RenameFailure(f"Erreur: Le nom saisie est vide et ne contient aucun caractères. Nom saisie : '{rename}'")
 
-    if name.strip() == "":
-        sys.exit("Erreur: Le nom saisie est vide et ne contient aucun caractère")
+    folder_existing_not_empty = existing_not_empty(conf.root_img_before)
 
-    for img in conf.root_img_before.iterdir():
-        img = str(img)
-        template_data = img_template.parse(img)
-        frame = template_data['frame']
+    if folder_existing_not_empty:
+        for img in conf.root_img_before.iterdir():
+            img = str(img)
+            template_data = img_template.parse(img)
+            frame = template_data['frame']
 
-        rename = (conf.root_img_after / f"{name}.{frame}.exr")
+            rename_path = (conf.root_img_after / f"{rename}.{frame}.exr")
 
-        shutil.copy2(img, rename)
+            shutil.copy2(img, rename_path)
 
 
-def RenameFailure(Exception):
+def check_existing_not_empty(folder):
+    """
+    Fonction permettant de vérifier si le dossier choisit par l'utilisateur est bien existant, est bien un dossier et non un fichier et contient bien des fichiers pour être renommés
+
+    :param folder: Dossier contenant les fichiers à renommer
+    :return: True si le dossier est existant n'est pas un fichier et contient bien des fichiers, sinon raise une erreur selon la situation
+    """
+    if folder.exists() and not folder.is_file():
+        if not list(folder.iterdir()):
+
+            raise RenameFailure("Erreur: Le dossier dans lequel vous cherchez est vide.")
+
+        else:
+            return True
+
+    else:
+        raise RenameFailure(f"Erreur: Le dossier dans lequel les fichiers sont n'a pas été trouvé. {folder}")
+
+
+class RenameFailure(Exception):
     pass
 
 
 if __name__ == '__main__':
-    print(rename_copy_before_save())
+    rename = input("Quel sera le nouveau nom de vos différentes frames ?\n")  # masterlayer.0230.exr
+    print(copy_before_save(rename))
+
+    # folder = conf.root_img_before
+    # print(empty(folder))
